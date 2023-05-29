d3627a361122fce895966a3d957632c40e878058
Evander Inacio <evander.20112@hotmail.com>
2023-05-28 22:52:04 -0300

    :sparkles: feat: Construction Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,40 @@
+'use client'
+
 import Sidebar from '@/components/Sidebar'
+import IconMenu from '@/components/icons/IconMenu'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [openSidebar, setOpenSidebar] = useState(false)
+
+  const closeSidebar = () => {
+    setOpenSidebar(false)
+  }
+
+  const handleClear = () => {
+
+  }
+
+  const handleNewChat = () => {
+
+  }
+
   return (
     <main className='flex h-screen bg-gpt-gray'>
-      <Sidebar>
-        sidebar
+      <Sidebar
+        open={openSidebar}
+        onClose={closeSidebar}
+        onClear={handleClear}
+        onNewChat={handleNewChat}
+      >
+        ...
       </Sidebar>
 
-      <section className='flex flex-col w-full'>
-        ...
+      <section className='flex flex-col w-full items-center'>
+        <button onClick={() => setOpenSidebar(true)}> 
+          <IconMenu width={24} height={24} /> 
+        </button>
       </section>
     </main>
   )
